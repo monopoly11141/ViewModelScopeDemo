3b487cc14e247cc8e3635bb121f241d457d0b4ce
monopoly11141 <monopoly1114@vt.edu>
2023-03-13 13:31:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/viewmodelscopedemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.viewmodelscopedemo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityViewModel : MainActivityViewModel
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        mainActivityViewModel.getUserData()
+
+        mainActivityViewModel.users.observe(this, Observer{ myUsers ->
+             myUsers.forEach{
+                 Log.d(TAG, "name is ${it.name}")
+             }
+        })
+    }
+}
